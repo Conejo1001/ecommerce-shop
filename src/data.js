225312da5d49f8b28ad73d0092c13f901be55fb4
Conejo1001 <jcarlos.coneio@hotmail.com>
2021-10-25 19:48:07 -0500

    Added New Data

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+      img: "https://cdn.shopify.com/s/files/1/2714/9310/products/130066_1_775x.jpg",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "f5fafd",
